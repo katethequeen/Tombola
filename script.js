c964ff606341bebd45c2f2158a6119a2752c96ee
katethequeen <cateresa4@gmail.com>
2024-09-16 21:54:38 +0200

    Adding New main Table

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
-//Creazione dei box per i numeri (da 1 a 76)
+//Creazione dei box per i numeri (da 1 a 90)
 
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
-const button = document.querySelector("button");
+//Create a table
 
-function createTable() {
-  for (let i = 1; i <= 76; i++) {
-    const numberBox = document.createElement("div");
-    numberBox.className = "number-box";
-    container.appendChild(numberBox);
-    const number = document.createElement("h3");
-    number.innerText = i;
-    numberBox.appendChild(number);
+function getTable() {
+  const table = document.createElement("table");
 
-    button.addEventListener("click", function () {
-      const randomNumber = Math.floor(Math.random() * 76) + 1;
-      if (randomNumber === numberBox) {
-        console.log("ci seiiii");
-      } else {
-        console.log("ma che stai dicendo?");
-      }
-    });
+  let number = 1;
+  let tdList = []; //Array dei numeri nelle colonne della tabella
+  let selectedNumb = []; //Array dei numeri gia usciti dall'estrazione
+
+  for (let i = 1; i <= 9; i++) {
+    let tr = document.createElement("tr");
+    for (let j = 1; j <= 10; j++) {
+      let tdNumber = document.createElement("td");
+      tdNumber.innerText = number;
+      number++;
+
+      tr.appendChild(tdNumber);
+      tdList.push(tdNumber);
+    }
+    table.appendChild(tr);
   }
+  container.appendChild(table);
+
+  /*-- Create BTN --*/
+  btn.onclick = () => {
+    let randomNmb;
+    do {
+      randomNmb = Math.floor(Math.random() * 91);
+    } while (selectedNumb.includes(randomNmb));
+
+    selectedNumb.push(randomNmb);
+    if (selectedNumb.length === 90) {
+      let h4El = document.createElement("h4");
+      h4El.style.textAlign = "center";
+      h4El.innerText = "You have Completed Tombola!";
+
+      container.appendChild(h4El);
+
+      h4El.style.position = "absolute";
+      h4El.style.top = "50%";
+      h4El.style.left = "50%";
+      h4El.style.transform = "translate(-50%, -50%)";
+      h4El.style.background = "red";
+      h4El.style.fontSize = "100px";
+      h4El.style.padding = "20px";
+      h4El.style.zIndex = "999";
+      h4El.style.boxShadow = "0 8px 20px 1px black";
+    }
+
+    const selectNmb = tdList[randomNmb];
+    selectNmb.style.backgroundColor = "#c4020d";
+    selectNmb.style.color = "#f5d0a1";
+  };
+}
+
+getTable();
+
+/*const button = document.querySelector("button"
+function createTable() {
+
+  button.addEventListener("click", function () {
+    const randomNumber = Math.floor(Math.random() * 90) + 1;
+    const numberBoxClass = document.querySelectorAll(".number-box");
+    const numberBoxh3 = document.querySelectorAll(".number-box h3");
+
+    const selectNum = numberBoxh3[randomNumber - 1];
+    numberBoxClass.style.backgroundColor = "green";
+  });
 }
 createTable();
-
+*/
 // Creazione bottone estrazione
